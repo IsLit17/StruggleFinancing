67c528f5364c1e392774eae9874a8bba236b1950
kulkarnisheel@gmail.com <kulkarnisheel@gmail.com>
2023-02-04 14:54:51 -0800

    changes to notification obj

--- HomeScreen.js
+++ HomeScreen.js
@@ -2,25 +2,36 @@ import React, { Component } from 'react';
 import {StyleSheet, View, Text, FlatList, TouchableOpacity, Image, Button, Alert} from "react-native";
 // About screen contains the text “You are on the about page” and a button.
 export default function HomeScreen({navigation}) {
+    function NotifObj(name, days) {
+        this.name = name;
+        this.days = days;
+    }
+    const objList = [new NotifObj("Electric", 2), new NotifObj("Water", 3)]
+    const exList = [1,2,3]
     const notification = () =>
         Alert.alert(
-        "Testing Alert",
-        "Stop spending money",
+        "Reminder",
+        "Have you already paid this?",
         [
           {
-            text: "Ok",
-            onPress: () => console.log("Ok Pressed"),
+            text: "Yes",
+            onPress: () => console.log("Yes Pressed"),
             style: "cancel"
           },
-          { text: "Nope", onPress: () => console.log("Nope Pressed") }
+          { text: "Dismiss", onPress: () => console.log("Dismiss Pressed") }
         ],
         { cancelable: false }
-      );
+        );
     return (
       <View style={styles.container}>
         <Text style= {styles.greenTxt}>Struggle Financing</Text>
         <Text style = {styles.blueTxt}>Notifications</Text>
-        <Button title="2-Button Alert" onPress={notification} />
+        {exList.map(x => <TouchableOpacity activeOpacity={0.7} style={{ backgroundColor: 'green', padding: 10, borderRadius: 4 }} onPress={notification}>
+            <Text style={{ color: '#fff', fontSize: 24 }}>due in {x} days</Text>
+        </TouchableOpacity>)}
+        {/* <TouchableOpacity activeOpacity={0.7} style={{ backgroundColor: 'green', padding: 10, borderRadius: 4 }} onPress={notification}>
+            <Text style={{ color: '#fff', fontSize: 24 }}>x => due in 2 days</Text>
+        </TouchableOpacity> */}
         <View style = {bottomStyle.container}>
         <Button
             title="Home"
@@ -54,6 +65,13 @@ const styles = StyleSheet.create({
         color: 'blue',
         fontWeight: 'bold',
         fontSize: 16,
+    },
+    bubble : {
+        // maxWidth: scale(250),
+        paddingHorizontal: 15,
+        paddingTop: 10,
+        paddingBottom: 15,
+        borderRadius: 20,
     }
 });
 
