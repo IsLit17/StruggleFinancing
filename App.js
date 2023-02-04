2ad7150b4cb42d385fd5780bc8021b866e047280
IsLit17 <kulkarnisheel@gmail.com>
2023-02-03 21:52:20 -0800

    added stack nav

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 export default function App() {
   return (
+    <NavigationContainer>
     <View style={styles.container}>
       <Text>LETS GET STRUGGLE FINANCING! - RizzGod</Text>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} options={{title: 'Welcome'}}/>
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const LoginScreen = ({navigation}) => {
+  return (
+    <Button
+      title="Login"
+      onPress={() =>
+        navigation.navigate('Profile', {name: 'Jane'})
+      }
+    />
+  );
+};
+const ProfileScreen = ({navigation, route}) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
 
 const styles = StyleSheet.create({
   container: {
