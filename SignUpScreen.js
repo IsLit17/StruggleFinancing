7a83789987bfc9bea504ae9b74d595f0bc776830
hiddenshadows <munozedgar510@gmail.com>
2023-02-04 16:40:11 -0800

    alert for password

--- SignUpScreen.js
+++ SignUpScreen.js
@@ -6,7 +6,7 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore"; 
 import { auth, db } from "./firebaseConfig";
 
-export async function signInWithEmail(email, password, navigation, firstname, lastname, onChangeLoading){
+export async function signInWithEmail(email, password, navigation, firstname, lastname, onChangeLoading, onChangeAlert){
   console.log('inside sign up with email')
   onChangeLoading(
     <View style = {styles.overlayLoadingContainer}>
@@ -32,38 +32,25 @@ export async function signInWithEmail(email, password, navigation, firstname, la
     })
     .catch((error) => {
       onChangeLoading(null)
-      console.log('error creating user ')
+      onChangeAlert(
+        Alert.alert(
+          "Cannot sign up",
+          "Make sure password is at least 6 characters long",
+          [
+            {
+              text: "Go Back",
+              onPress: () => console.log("Ok Pressed") && onChangeAlert(null),
+              style: "cancel"
+            }
+          ],
+          { cancelable: false }
+        )
+      )
+      console.log('error creating user ', error)
       const errorCode = error.code;
       const errorMessage = error.message;
       // ..
     });
-  // await signInWithEmailAndPassword(auth, email, password)
-  //   .then((userCredential) => {
-  //     // Signed in 
-  //     const user = userCredential.user;
-  //     navigation.navigate('Home')
-  //     onChangeLoading(null)
-  //   })
-  //   .catch((error) => {
-  //     console.log('error login')
-  //     onChangeLoading(null)
-  //     onChangeAlert(
-  //       Alert.alert(
-  //         "something went wrong",
-  //         "Invalid password or email",
-  //         [
-  //           {
-  //             text: "Go Back",
-  //             onPress: () => console.log("Ok Pressed") && onChangeAlert(null),
-  //             style: "cancel"
-  //           }
-  //         ],
-  //         { cancelable: false }
-  //       )
-  //     )
-  //     const errorCode = error.code;
-  //     const errorMessage = error.message;
-  //   });
   return;
 }
 
@@ -79,71 +66,56 @@ export default function SignUpScreen({navigation}) {
   const [loading, onChangeLoading] = React.useState(null);
   const [alert, onChangeAlert] = React.useState(null);
 
-
-    // onChangeAlert(
-    //   Alert.alert(
-    //     "Invalid Credentials",
-    //     "Invalid password or email",
-    //     [
-    //       {
-    //         text: "Go Back",
-    //         onPress: () => console.log("Ok Pressed") && onChangeAlert(null),
-    //         style: "cancel"
-    //       }
-    //     ],
-    //     { cancelable: false }
-    //   )
-    // )
-    return (
-      <View style={styles.container}>
-        <Text style = {{top: 15, fontSize: 38, color: '#072'}}>Spend Smarter</Text>
-        <Text style = {{top: 17.5, fontSize: 37, color: '#072'}}>With</Text>
-        <Text style = {{top: 20, fontSize: 35, color: '#072', fontWeight: 'bold'}}>Struggle Financing</Text>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-          <View style={{flex: 1, margin: 10, padding: 20}}>
-            <TextInput 
-              style={textboxStyle.input}
-              onChangeText={onChangeFirst}
-              value={first}
-              placeholder="First Name"
-            />
-            <TextInput
-              style={textboxStyle.input}
-              onChangeText={onChangeLast}
-              value={last}
-              placeholder="Last Name"
-            />
-            <TextInput
-              style={textboxStyle.input}
-              keyboardType="email-address"
-              onChangeText={onChangeEmail}
-              value={email}
-              placeholder="Email"
-            />
-            <TextInput
-              style={textboxStyle.input}
-              onChangeText={onChangePassword1}
-              value={password1}
-              placeholder="Password"
-            />
-            <TextInput
-              style={textboxStyle.input}
-              onChangeText={onChangePassword2}
-              value={password2}
-              placeholder="Confirm Password"
-            />
-          </View>
-        </TouchableWithoutFeedback>
-        <Text style = {{bottom: 170, fontSize: 25}}>Bronco's Country {""}
-          <Text style={{color: 'blue', textDecorationLine: 'underline'}}
-            onPress={() => signInWithEmail(email, password1, navigation.navigate("Subs"), first, last, onChangeLoading)}>
-            Lets Ride
-          </Text>
+  return (
+    <View style={styles.container}>
+      <Text style = {{top: 15, fontSize: 38, color: '#072'}}>Spend Smarter</Text>
+      <Text style = {{top: 17.5, fontSize: 37, color: '#072'}}>With</Text>
+      <Text style = {{top: 20, fontSize: 35, color: '#072', fontWeight: 'bold'}}>Struggle Financing</Text>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View style={{flex: 1, margin: 10, padding: 20}}>
+          <TextInput 
+            style={textboxStyle.input}
+            onChangeText={onChangeFirst}
+            value={first}
+            placeholder="First Name"
+          />
+          <TextInput
+            style={textboxStyle.input}
+            onChangeText={onChangeLast}
+            value={last}
+            placeholder="Last Name"
+          />
+          <TextInput
+            style={textboxStyle.input}
+            keyboardType="email-address"
+            onChangeText={onChangeEmail}
+            value={email}
+            placeholder="Email"
+          />
+          <TextInput
+            style={textboxStyle.input}
+            onChangeText={onChangePassword1}
+            value={password1}
+            placeholder="Password"
+          />
+          <TextInput
+            style={textboxStyle.input}
+            onChangeText={onChangePassword2}
+            value={password2}
+            placeholder="Confirm Password"
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Text style = {{bottom: 170, fontSize: 25}}>Bronco's Country {""}
+        <Text style={{color: 'blue', textDecorationLine: 'underline'}}
+          onPress={() => signInWithEmail(email, password1, navigation, first, last, onChangeLoading, onChangeAlert)}>
+          Lets Ride
         </Text>
+      </Text>
 
-        
-      </View>
-    );
+      
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
